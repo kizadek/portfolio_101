ddfdd009381e8ba2069e623cea3542e41f45a752
kizadek <48857035+kizadek@users.noreply.github.com>
2021-12-22 11:48:45 +0200

    technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import {FaHammer} from 'react-icons/fa'
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import { FaHammer } from "react-icons/fa";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
- <Section id="tech">
-   <SectionDivider divider />
-   <SectionTitle>Technologies</SectionTitle>
-   <SectionText>
-     <SectionText>
-     I've worked with a range a technologies in the web development world.
-      From Back-end To Design
-     </SectionText>
-     <List>
-       <ListItem>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      <SectionText>
+        I've worked with a range a technologies in the web development world.
+        From Back-end To Design
+      </SectionText>
+      <List>
+        <ListItem>
           <picture>
             <DiFirebase size="4rem" />
           </picture>
@@ -25,33 +36,31 @@ const Technologies = () =>  (
               Node, Express and Databases
             </ListParagraph>
           </ListContainer>
-       </ListItem>
-       <ListItem>
-         <picture>
-           <DiReact size="4rem" />
-         </picture>
-         <ListContainer>
-           <ListTitle>FRONT-END</ListTitle>
-           <ListParagraph>
-             Great with <br />
-             React.js 
-           </ListParagraph>
-         </ListContainer>
-       </ListItem>
-       <ListItem>
-         <picture>
-           <FaHammer size="3rem" />
-           <ListContainer>
-             <ListTitle>UI/UX</ListTitle>
-             <ListParagraph>
-               The best ideas take root 
-             </ListParagraph>
-           </ListContainer>
-         </picture>
-       </ListItem>
-     </List>
-   </SectionText>
- </Section>
+        </ListItem>
+        <ListItem>
+          <picture>
+            <DiReact size="4rem" />
+          </picture>
+          <ListContainer>
+            <ListTitle>FRONT-END</ListTitle>
+            <ListParagraph>
+              Great with <br />
+              React.js
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <picture>
+            <FaHammer size="3rem" />
+            <ListContainer>
+              <ListTitle>UI/UX</ListTitle>
+              <ListParagraph>The best ideas take root</ListParagraph>
+            </ListContainer>
+          </picture>
+        </ListItem>
+      </List>
+    </SectionText>
+  </Section>
 );
 
 export default Technologies;
