4584a7b0f4a5a1e52962a1f35cd28918051edfdb
kizadek <48857035+kizadek@users.noreply.github.com>
2021-12-28 15:15:50 +0200

    simple

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <>
     <Section row nopadding>
       <LeftSection>
-        <SectionTitle>
-          <span style={{color:"#ccdee4",fontWeight: "bold"}}>Hi ! (●'◡'●)</span> <br />
-         <span style={{color:"#ccdee4",fontWeight: "lighter"}}>I’am Nsowa Mupela </span> 
+        <SectionTitle main center>
+          <span style={{ color: "#ccdee4", fontWeight: "bold" }}>Hi !</span>{" "}
+          <br />
+          <span style={{ color: "#ccdee4", fontWeight: "lighter" }}>
+            I’am Nsowa Mupela{" "}
+          </span>
         </SectionTitle>
         <SectionText>
-        <span style={{color: ""}}>
-        <span style={{fontSize:"50px"}}>A</span> software Engineer/Designer<br /> based in Africa Zambia,
-        loves to craft attractive useful interactive products for the web. 
-        introduced to computer scienc in 2017,</span><br /> <span style={{color:"",fontSize:"2rem"}}>if you are a business seeking for web presence or an employer looking to hire, get in touch with me</span>
+          <span style={{ color: "" }}>
+            <span style={{ fontSize: "50px" }}>A</span> software
+            Engineer/Designer
+            <br /> based in Africa Zambia, loves to craft attractive useful
+            interactive products for the web.
+          </span>
+          <br />{" "}
+          <span style={{ color: "", fontSize: "2rem" }}>
+            if you are a business seeking for web presence or an employer
+            looking to hire, get in touch with me
+          </span>
         </SectionText>
         <Button onClick={props.handleClick}>MORE ABOUT ME</Button>
       </LeftSection>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -60,6 +60,7 @@ const Technologies = () => (
         </ListItem>
       </List>
     </SectionText>
+    <SectionDivider />
   </Section>
 );
 
